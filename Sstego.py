6efd1cd64ecb2a78f78379d66d2e36142918d676
Anibal Cañada <anibal@n2h4.es>
2021-03-25 18:08:01 +0100

    v0.01 - acepta argumentos y subido primer README.md

---
+++ Sstego.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# coding:UTF-8
+
+"""SStego.py
+
+Usage:
+  SStego.py encode -i <input> -o <output> -f <file> -p password
+  SStego.py decode -i <input> -o <output> -p password
+
+Options:
+  -h, --help                Show this help
+  --version                 Show the version
+  -f,--file=<file>          File to hide
+  -i,--in=<input>           Input image (carrier)
+  -o,--out=<output>         Output image (or extracted file)
+
+"""
+from docopt import docopt
+
+def main() :
+	args = docopt(__doc__, version='0.1')
+	in_f = args["--in"]
+	out_f = args["--out"]
+
+
+	if args['encode'] :
+		print ("encode")
+
+
+	elif args["decode"] :
+		print("decode")
+
+
+if __name__ == '__main__' :
+	main ()
